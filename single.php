94a265716decfcff2499dc1d8c471187c07d666a
Saurav Kumar <saurav@dezyne.in>
2016-12-05 14:24:18 +0530

    ideataru blog edit

--- single.php
+++ single.php
@@ -8,28 +8,86 @@
  */
 
 get_header(); ?>
+<!-- Hero Area -->
+<div class="hero-area">
+	<div class="page-banner parallax" style="background-image:url(images/inside8.jpg);">
+			<div class="container">
+					<div class="page-banner-text">
+					<h1 class="block-title">Blog</h1>
+						</div>
+				</div>
+		</div>
+</div>
+<!-- Main Content -->
+<div id="main-container">
+	<div class="content">
+			<div class="container">
+					<div class="row">
+							<div class="col-md-8 content-block">
+								<?php
+	while ( have_posts() ) : the_post();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		get_template_part( 'template-parts/content', get_post_format() );
 
-		<?php
-		while ( have_posts() ) : the_post();
+		the_post_navigation();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 
-			the_post_navigation();
+	endwhile; // End of the loop.
+	?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+								   <!-- Post Title Goes Here -->
+									 <!--
+									<h3>A single person can change million lives</h3> -->
 
-		endwhile; // End of the loop.
-		?>
+									  <!-- <div class="post-media">
+											<img src="images/post-detail1.jpg" alt="">
+										</div> -->
+										<!-- Post Content -->
+										<!--
+										<div class="post-content">
+											<p>Vestibulum quam nisi, pretium a nibh sit amet, consectetur hendrerit mi. Aenean imperdiet lacus sit amet elit porta, et malesuada erat bibendum. Cras sed nunc massa. Quisque tempor dolor sit amet tellus malesuada, malesuada iaculis eros dignissim. Aenean vitae diam id lacus fringilla maximus. Mauris auctor efficitur nisl, non blandit urna fermentum nec.</p>
+											<img src="images/cause3.jpg" alt="" class="align-left">
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa ipsum, efficitur a fermen tum sed, suscipit sit amet arcu. Ut ut finibus tortor, eu ultrices turpis. Mauris vitae elit nec diam elementum elementum. Mauris ante quam, consequat ac nibh placerat, lacinia sollicitudin mi. Duis facilisis nibh quam, sit amet interdum tellus sollicitudin tempor. Curabitur aliquam erat in nisl lobortis, ut pellentesque lectus viverra.</p>
+											<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa ipsum, efficitur a fermen tum sed, suscipit sit amet arcu. Ut ut finibus tortor, eu ultrices turpis. Mauris vitae elit nec diam elementum elementum. Mauris ante quam, consequat ac nibh placerat, lacinia sollicitudin mi. Duis facilisis nibh quam, sit amet interdum tellus sollicitudin tempor. Curabitur aliquam erat in nisl lobortis, ut pellentesque lectus viverra. Aenean sodales aliquet arcu at aliquam. Vestibulum quam nisi, pretium a nibh sit amet, consectetur hendrerit mi. Aenean imperdiet lacus sit amet elit porta, et malesuada erat bibendum. Cras sed nunc massa. Quisque tempor dolor sit amet tellus malesuada, malesuada iaculis eros dignissim. Aenean vitae diam id lacus fringilla maximus. Mauris auctor efficitur nisl, non blandit urna fermentum nec.</p>
+										</div> -->
+										<!-- Tags -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+										<!-- <div class="tag-cloud">
+												<i class="fa fa-tags"></i>
+												<a href="#">Water</a>
+												<a href="#">Students</a>
+												<a href="#">NYC</a>
+										</div> -->
+										<!-- About Author -->
+										<!-- <section class="about-author">
+												<img src="images/user1.jpg" alt="avatar" class="img-thumbnail">
+												<div class="post-author-content">
+														<h3>Emma Paquette <span class="label label-primary">Author</span></h3>
+														<p><strong>Beverly Barnett</strong>, is a regular writer for the Vestige Museum blog. She loves to write more about the animal species and have so much faith in the nature's doing.</p>
+												</div>
+										</section> -->
+										<!-- Pagination -->
+										<!-- <ul class="pager">
+												<li class="pull-left"><a href="#">&larr; Prev Post</a></li>
+												<li class="pull-right"><a href="#">Next Post &rarr;</a></li>
+										</ul> -->
+										<!-- Comment Block -->
+										<!-- Comment Form -->
+								</div>
 
+								<!-- Sidebar -->
+								<?php
+								get_sidebar();
+								?>
+						</div>
+				</div>
+		</div>
+</div>
+<!-- Site Footer -->
 <?php
-get_sidebar();
 get_footer();
+?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -4,44 +4,63 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package ideataru
+ * @package vedkrishna
  */
-
 ?>
+<div class="blog-list-item format-standard">
+	<!-- <div class="container"> -->
+		<div class="row">
+				  <div class="col-md-4 col-sm-4">
+						<?php if ( has_post_thumbnail() ) : ?>
+								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="media-box grid-featured-img">
+									<?php the_post_thumbnail(); ?>
+								</a>
+							<?php endif; ?>
+						<!-- <a href="<?php esc_url( get_permalink() ); ?>" class="media-box grid-featured-img">
+									<img src="<?php bloginfo('template_directory'); ?>/images/post1.jpg" alt="">
+							</a>-->
+					</div>
+					<div class="col-md-8 col-sm-8">
+							<?php
+							if ( is_single() ) :
+								the_title( '<h2 class="entry-title">', '</h1>' );
+							else :
+								the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+							endif;
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+							if ( 'post' === get_post_type() ) : ?>
+							<div class="entry-meta">
+								<?php vedkrishna_posted_on(); ?>
+							</div><!-- .entry-meta -->
+							<?php
+							endif; ?>
+							<!-- <div class="grid-item-excerpt">
+									<p>A blog post sample excerpt text which can be edited by editing the blog post page. Excerpt length can be changed from the default blog settings...</p>
+							</div> -->
+							<div class="grid-item-excerpt">
+								<?php
+								  the_excerpt();
+									/* translators: %s: Name of current post. */
+									/*
+									the_content( sprintf(
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php ideataru_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+										wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'vedkrishna' ), array( 'span' => array( 'class' => array() ) ) ),
+										the_title( '<span class="screen-reader-text">"', '"</span>', false )
+									) );*/
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'ideataru' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ideataru' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php ideataru_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+									wp_link_pages( array(
+										'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vedkrishna' ),
+										'after'  => '</div>',
+									) );
+								?>
+							</div><!-- .entry-content -->
+							<?php vedkrishna_entry_footer(); ?>
+							<!--
+							<footer class="entry-footer">
+								<?php vedkrishna_entry_footer(); ?>
+							</footer> --> <!-- .entry-footer -->
+							<a href="<?php esc_url( get_permalink()); ?>" class="basic-link">Read more</a>
+					</div>
+			</div>
+		<!--</div>-->
+	</div>
